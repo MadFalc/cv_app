0e60469cae6d8750929e3d6c75344d39786d0023
MadFalc <68642371+MadFalc@users.noreply.github.com>
2023-01-04 22:56:33 +0100

    Alles gedaan volgens het boekje
    Morgenvroeg even aan Tomi vragen of dit voldoende 'leren' is. Want dit voelt niet direct alsof ik er veel van opsteek

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// INCLUDE THE CONTROLLERS YOU NEED HERE.
+use App\HTTP\Controllers\PersonController;
+
+// HERE I REDIRECT MY DEFAULT ROUTE TO MY PERSONS.
+Route::get('/', [PersonController::class, 'index']);
+
+// THE DEFAULT LARAVEL ROUTES ARE: INDEX, SHOW, CREATE, SHOW, EDIT, UPDATE, DESTROY
+// THE ROUTE STRUCTURE LOOKS LIKE THIS: Route::get('url', [Controller::class, 'function']);
+// YOU CAN USE THE PHP ARTISAN ROUTE LIST FUNCTION TO SHOW ALL YOUR ROUTES IN THE CONSOLE.
+Route::get('/person', [PersonController::class, 'index'])->name('person.index');
+
+// ROUTE RESOURCES ARE USED TO COMBINE ALL YOUR BASIC ROUTES.
+Route::resource('/person', PersonController::class);
